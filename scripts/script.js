fb8502ef94e07153546717dfed0d1816d806b3e3
unknown <jtroiani@LENOVOT440P-49.Justgiving.local>
2014-12-04 08:46:21 +0000

    asks for a location and gives you the weather in 3 varaiations

---
+++ scripts/script.js
@@ -0,0 +1,58 @@
+
+
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: 'forecast.html',
+            controller: 'forecastController'
+        })
+        .when('/forecast/:days', {
+            templateUrl: 'forecast.html',
+            controller: 'forecastController'
+        });
+});
+
+
+weatherApp.service('cityService', function () {
+    this.city = "Brighton, East Sussex";
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.cityName = cityService.city;
+    $scope.$watch('cityName', function () {
+        cityService.city = $scope.cityName;
+    });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
+    $scope.cityName = cityService.city;
+	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
+		callback: 'JSON_CALLBACK'},
+		{get : {method: 'JSONP'}});
+	$scope.days = $routeParams.days || 3
+	$scope.weatherResult = $scope.weatherAPI.get({q: $scope.cityName , cnt: $scope.days });	
+
+    $scope.toCelcius = function(kelvin){
+        return Math.round(kelvin - 273.15);
+        };
+    $scope.toDay = function(date){
+        return new Date(date*1000);
+        };
+
+    }                                            
+]);
+
+weatherApp.directive('weather',function(){
+   return{
+       templateUrl:'directives/weather.html',
+       restrict:'E',
+       replace:'true'
+   } 
+});
+
